9461a7b625cf56fea88ab25415d29e51bd7b5b5c
Mateus <mateus.gueler@gmail.com>
2022-04-09 15:05:02 -0300

    jogo finalizado e com o reloginho

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
+// VARIÁVEIS GLOBAIS
+
+let numeroDeCartas;
+
+let cartaVirada = 0;
+
+let localCarta1;
+
+let localCarta2;
+
+let carta1;
+
+let carta2;
+
+let acertos = 0;
+
+let jogadas = 0;
+
+let segundos = 0;
+
+let minutos = 0;
+
+let horas = 0;
+
+let contar;
+
+
+
+//CRONOMETRO
+
+function timer(){
+    let acharTempo = document.querySelector(".relogio")
+    
+    if(segundos<59){
+        segundos ++
+    }
+    else{
+        segundos = 0;
+        minutos++
+    }
+
+    acharTempo.innerHTML = minutos + " : " + segundos;
+}
+
+function cronometro(){
+    contar = setInterval(timer,1000)
+}
+
+
+
+// FUNÇÃO PARA DETERMINAR O NÚMERO DE CARTAS
+
 function comparador() { 
 	return Math.random() - 0.5; 
 }
@@ -23,6 +75,8 @@ function quantidadeCartas(){
         cartas = Number(prompt("Quantas cartas"));
 
     }
+
+    numeroDeCartas = cartas;
 
     const gif = [
     'bobrossparrot.gif','bobrossparrot.gif',
@@ -52,38 +106,106 @@ function quantidadeCartas(){
     }
 
     embaralhado = []
+
+    cronometro()
 }
 
+
+
+// ACERTOU E ERROU
+
+function acertou(){
+    acertos += 2;
+    let result = numeroDeCartas-acertos
+
+    if(result<2){
+        alert("PARABÉNS!!! \nVocê ganhou em " + jogadas + " jogadas \n" +
+         "Seu tempo foi de:\n" +
+         minutos +" minutos"+ " e " + segundos + " segundos")
+        // const reiniciar = prompt("Quer jogar novamente?")
+        clearInterval(contar)
+        segundos = 0;
+    }
+    
+}
+
+function errou(){
+    console.log("errou")
+    localCarta1.classList.add("retomar")
+    localCarta1.classList.remove("girar")
+    localCarta1.classList.remove("virada")
+
+    localCarta2.classList.add("retomar")
+    localCarta2.classList.remove("girar")
+    localCarta2.classList.remove("virada")
+}
+
+
+
+// FUÇÃO PARA VIRAR AS CARTAS
 
 function virarCarta(item){
 
     const temFlip = item.classList.contains("girar")
 
-    if(temFlip === false){
-        item.classList.add("girar")
-        item.classList.remove("retomar")
+    const estaVirada = item.classList.contains("virada")
 
+    if (cartaVirada === 0 && estaVirada === false){
+        cartaVirada = cartaVirada + 1;
+
+
+        if(temFlip === false){
+            item.classList.add("girar")
+            item.classList.remove("retomar")
+
+            item.classList.add("virada")
+
+            localCarta1 = item;
+
+            carta1 = item.innerHTML
+
+            console.log(item)
+
+            console.log("primeira carta ->" + cartaVirada)
+
+            cartaVirada ++
+
+            jogadas++
+        }
     }
     else{
-        item.classList.add("retomar")
-        item.classList.remove("girar")
+        if(cartaVirada !== 0 && estaVirada === false){
+
+            item.classList.add("girar")
+            item.classList.remove("retomar")
+            item.classList.add("virada")
+
+            localCarta2 = item;
+    
+            carta2 = item.innerHTML
+
+            if(carta2 == carta1){
+                setTimeout(acertou,1000)
+        
+            }
+        
+            else{
+                console.log("Errou")
+                setTimeout(errou,1000)
+        
+            }
+    
+            console.log("segunda carta ->" + cartaVirada)
+            cartaVirada = 0;
+            carta1 = ""
+            carta2 = ""
+            jogadas++
+        }
     }
 
 }
 
-function selecionarCarta(elemento){
-    const imagem = ['unicornparrot.gif','revertitparrot.gif']
 
-    elemento.classList.add("virada")
-    elemento.classList.add("carta-selecionada")
-    elemento.innerHTML = '<img src="/Imagens/'+imagem[1]+'"></img>'
-
-    const verCartasViradas = document.querySelectorAll(".virada")
-    alert(verCartasViradas.innerHTML)
-}
+// CHAMAR AS FUNÇÕES
 
 quantidadeCartas()
-
-// const gif = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif']
-
-// let m = gif.sort();
